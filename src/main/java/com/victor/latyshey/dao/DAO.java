86e6d016b3b2f5f5ccdd92a74b7736aff1b5ae7a
VictorLay <Viktor.latyshey@mail.ru>
2022-05-14 22:54:57 +0300

    Description for main C.R.U.D. operation of Data Access Layer.

---
+++ src/main/java/com/victor/latyshey/dao/DAO.java
@@ -0,0 +1,16 @@
+package com.victor.latyshey.dao;
+
+import com.victor.latyshey.beans.Entity;
+import com.victor.latyshey.dao.exception.DaoException;
+
+public interface DAO<Type extends Entity> {
+
+  Integer create(Type entity) throws DaoException;
+
+  Type read(Integer id) throws DaoException;
+
+  void update(Type entity) throws DaoException;
+
+  void delete(Integer id) throws DaoException;
+
+}
