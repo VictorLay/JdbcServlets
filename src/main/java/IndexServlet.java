b3c4e45acbc65f8ecf9196ee9bb0aabe8b862eda
VictorLay <Viktor.latyshey@mail.ru>
2022-05-26 12:58:27 +0300

    classes for simple tests

--- src/main/java/IndexServlet.java
+++ src/main/java/IndexServlet.java
@@ -9,10 +9,8 @@ import java.io.IOException;
 //@WebServlet("/")
 public class IndexServlet extends HttpServlet {
 
-  private final static String INDEX = "/WEB-INF/view/index.jsp";
-  private final static String REGISTRATION = "/WEB-INF/view/registration_page.jsp";
   private final static String MENU = "/WEB-INF/view/user_menu.jsp";
-  private final static String GREETING = "/WEB-INF/view/greeting_page.jsp";
+  private final static String SIMPLE = "/WEB-INF/view/book/simple.jsp";
 
   @Override
   public void init() throws ServletException {
@@ -22,7 +20,7 @@ public class IndexServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    getServletContext().getRequestDispatcher(REGISTRATION).forward(req, resp);
+    getServletContext().getRequestDispatcher(SIMPLE).forward(req, resp);
 
 //    resp.sendRedirect(req.getContextPath() + INDEX);
   }
@@ -31,10 +29,6 @@ public class IndexServlet extends HttpServlet {
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
 
-    String name = req.getParameter("forename");
-    String surname = req.getParameter("surname");
-    String username = req.getParameter("username");
-    String password = req.getParameter("password");
 
     System.out.println("creating");
 
