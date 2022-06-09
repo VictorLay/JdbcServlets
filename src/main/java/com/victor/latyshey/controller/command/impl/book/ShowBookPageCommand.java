2a5220c7944b27c4d03a3bd7ee956ac22b430927
VictorLay <Viktor.latyshey@mail.ru>
2022-06-09 19:59:22 +0300

    added logger and appropriate error page

--- src/main/java/com/victor/latyshey/controller/command/impl/book/ShowBookPageCommand.java
+++ src/main/java/com/victor/latyshey/controller/command/impl/book/ShowBookPageCommand.java
@@ -3,31 +3,29 @@ package com.victor.latyshey.controller.command.impl.book;
 import com.victor.latyshey.bean.book.Book;
 import com.victor.latyshey.controller.command.Command;
 import com.victor.latyshey.controller.command.CommandResponse;
-import com.victor.latyshey.dao.exception.DaoException;
-import com.victor.latyshey.dao.transaction.TransactionFactory;
 import com.victor.latyshey.service.BookService;
 import com.victor.latyshey.service.exception.ServiceException;
 import com.victor.latyshey.service.impl.BookServiceImpl;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.victor.latyshey.util.ResourceManager;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ShowBookPageCommand implements Command {
+  private static final Logger logger = LogManager.getLogger(ShowBookPageCommand.class);
 
   @Override
   public CommandResponse execute(HttpServletRequest req, HttpServletResponse resp) {
-    BookService bookService = null;
     try {
-      bookService = new BookServiceImpl(TransactionFactory.getInstance().getTransaction());
+      BookService bookService = new BookServiceImpl();
       Integer bookId = Integer.parseInt(req.getParameter("book_id"));
       Book book = bookService.showBook(bookId);
       req.setAttribute("book", book);
-    } catch (DaoException | ServiceException e) {
-      //todo add error log
-    } finally {
-      if (bookService != null) {
-        bookService.releaseTheConnection();
-      }
+    } catch (ServiceException e) {
+      logger.log(Level.ERROR, e);
+      return new CommandResponse(ResourceManager.getProperty("page.common_error"), false);
     }
 
     return new CommandResponse(ResourceManager.getProperty("page.book_showing"), false);
